ea3a613a61c2fbb5baac33c81343089c53f81fa5
Steve <steve@geoint.org>
2026-02-23 00:21:49 -0500

    fix mypy stuff. fr this time

--- grdl_rt/execution/operators.py
+++ grdl_rt/execution/operators.py
@@ -76,7 +76,7 @@ class WorkflowOperator(ImageProcessor):
         -------
         tuple[Any, ImageMetadata]
         """
-        self._metadata: ImageMetadata = metadata
+        self._metadata = metadata  # type: ignore[assignment]
         return self.operate(metadata, source, **kwargs)
 
     @abstractmethod
